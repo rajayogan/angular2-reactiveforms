bddfb72d27d9b1f837ad27caaed15e01def09cbc
rajayogan <rajayogan27@gmail.com>
2016-11-28 17:53:01 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+import { Contacts } from './contacts.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  contactinfo: FormGroup;
+  
+  constructor(private formbuilder: FormBuilder) {}
+  
+  ngOnInit() {
+    this.contactinfo = this.formbuilder.group({
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      contactno: ['', [Validators.required, Validators.minLength(7)]]
+    });
+  }
+  
+  onSubmit({value, valid}: {value: Contacts, valid: boolean}) {
+    console.log(value, valid);
+  }
 }
